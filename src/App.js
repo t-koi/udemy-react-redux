abdfc5f1e3e2593cf0948925f35019a62d0ca08e
koiduka tatsuki <>
2021-07-09 14:04:14 +0900

    aaaaaa

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render(){
-    const str = "hi tmo";
-    const dom = 
-        <React.Fragment>
-          <label htmlFor="bar">bar</label>
-          <input type="text" onClick={()=> {console.log("click")} }/>
-        </ React.Fragment>
-    return dom ;
-  }
-};
+// class App extends Component {
+//   render(){
+//     const str = "hi tmo";
+//     const dom = 
+//         <React.Fragment>
+//           <label htmlFor="bar">bar</label>
+//           <input type="text" onClick={()=> {console.log("click")} }/>
+//         </ React.Fragment>
+//     return dom ;
+//   }
+// };
 
+const App = () => {
+  return (
+    <div> 
+      <cat />
+      <cat />
+      <cat />
+    </div>)
+}
+
+const cat = () => {
+  return <div>Move!</div>
+}
 export default App;
  
